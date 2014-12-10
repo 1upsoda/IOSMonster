8391e5d4c95d7ce6dc68cf79a6e2c05830b6957d
1upsoda <1upsoda@gmail.com>
2014-12-10 08:09:20 -0700

    Adding the begginings
    adding the start to my IOS monster, instead of having it only java based

---
+++ Model/MashmellowMonster.swift
@@ -0,0 +1,80 @@
+public class MarshmellowMonster
+{
+	var name :String
+	var numberOfLegs :Double
+	var hairCount :Double
+	var eyeCount, noseCount, armCount :Int
+	var hasBellyButton :Bool
+	
+	init(name :String, numberOfLegs :Double)
+	{
+		self.name = name
+		self.numberOfLegs = numberOfLegs
+	}	
+	
+	init(name :String, numberOfLegs :Double, hairCount :Int, noseCount :Int, armCount :Int, hasBellyButton :Bool)
+	{
+		self.name = name
+		self.numberOfLegs = numberOfLegs
+		self.hairCount = hairCount
+		self.eyeCount = eyeCount
+		self.noseCount = noseCount
+		self.armCount = armCount
+		self.hasBellyButton = hasBellyButton
+	}
+	
+	func getName() -> String
+	{
+		return name
+	}
+	
+	func getNumbeOfLegs() -> Double
+	{
+		return numberOfLegs
+	}
+	func getHairCount() -> Double
+	{
+		return hairCount
+	}
+	func getEyeCount() -> Int
+	{
+		return eyeCount
+	}
+	func getNoseCount() -> Int
+	{
+		return noseCount
+	}
+	func getArmCount() -> Int
+	{
+		return armCount
+	}
+	func getHasBellyButton() -> Bool
+	{
+		return hasBellyButton
+	}
+	
+	func setName(name :String)
+	{
+		self.name = name
+	} 
+	
+	func setNumberOfLegs(numberOfLegs :Double)
+	{
+		self.numberOfLegs = numberOfLegs
+	} 
+	
+	func setHairCount(hairCount :Double)
+	{
+		self.hairCount = hairCount
+	}
+	
+	func setEyeCount (eyeCount :Int)
+	{
+		self.eyeCount = eyeCount
+	}
+	
+	
+	
+	
+	
+}
